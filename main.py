4456216aece65ca6a3286a869e09e9bbff500fda
AlexDev <alex_soto@ciisa.cl>
2022-04-29 00:46:44 -0400

    60 min.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 nombre = "alex"
 edad=34
-
 print("Hola " + nombre + ' y tengo :' + str(edad) + ' años')
+
+nombre2= input('escribe nombre')
+
+print('escribiste : ' + nombre2)
+
+tuEdad= int(input('ingresa edad'))
+
+if edad == tuEdad:
+    print("Siii")
+else:
+    print("Noooo")
+
+if(tuEdad % 2 != 0):
+    print("EDAD ES IMPAR")
+else:
+    print("EDAD ES PAR")
