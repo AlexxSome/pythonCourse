a482b719b3ba051ea6b7acb0fd9896ca7ef87854
AlexDev <alex_soto@ciisa.cl>
2022-04-29 01:03:59 -0400

    calculator IMC min. 80

---
+++ calculatorIMC.py
@@ -0,0 +1,11 @@
+peso = int(input("ingresa peso en kg: "))
+altura = int(input("ingresa altura en cm: "))
+altura = altura / 100
+imc = peso / (altura * altura)
+print("IMC : " + str(imc))
+if imc < 20:
+    print("Delgadez")
+if imc >= 20 and imc < 26:
+    print("Normal")
+if imc >= 26 and imc < 30:
+    print("Sobrepeso")
